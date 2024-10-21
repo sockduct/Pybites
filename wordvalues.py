e1e1f9ea9ded508bf85b25ed1286ed4427e39acf
James Small <james.r.small@outlook.com>
2024-10-20 22:16:59 -0400

    Update:  Finish building out functions in wordvalues

--- wordvalues.py
+++ wordvalues.py
@@ -10,6 +10,7 @@ TMP = os.getenv('TMP', TMPDIR)
 
 CWD = Path(__file__).parent
 S3 = 'https://bites-data.s3.us-east-2.amazonaws.com/'
+DATA = 'data'
 DICT = 'dictionary.txt'
 
 scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
@@ -22,7 +23,7 @@ LETTER_SCORES = {letter: score for score, letters in scrabble_scores
 def get_dict():
     # DICTIONARY = os.path.join(TMP, DICT)
     # dictionary = Path(TMP)/DICT
-    dictionary_dir = CWD/'data'
+    dictionary_dir = CWD/DATA
     dictionary_file = dictionary_dir/DICT
 
     if not dictionary_dir.exists():
@@ -36,17 +37,23 @@ def get_dict():
 
 def load_words():
     """Load the words dictionary (DICTIONARY constant) into a list and return it"""
-    pass
+    with open(CWD/DATA/DICT) as infile:
+        word_list = infile.read().splitlines()
+
+    word_set = set(word_list)
+
+    return word_list, word_set
 
 
 def calc_word_value(word):
     """Given a word calculate its value using the LETTER_SCORES dict"""
-    pass
+    return sum(LETTER_SCORES.get(letter.upper(), 0) for letter in word)
 
 
 def max_word_value(words):
     """Given a list of words calculate the word with the maximum value and return it"""
-    pass
+    return max(calc_word_value(word) for word in words)
 
 if __name__ == '__main__':
     get_dict()
+    word_list, word_set = load_words()
