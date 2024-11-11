9a87b635d76582ea2e8f44639ecad3ce4e9e31dd
James Small <james.r.small@outlook.com>
2024-11-10 22:50:05 -0500

    Update:  Complete parsing dates from logs challenge

--- Intermediate/logparsedt.py
+++ Intermediate/logparsedt.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 import urllib.request
 
@@ -36,7 +36,7 @@ def get_data() -> list[str]:
     return loglines
 
 
-def convert_to_datetime(line):
+def convert_to_datetime(line: str) -> datetime:
     """TODO 1:
        Extract timestamp from logline and convert it to a datetime object.
        For example calling the function with:
@@ -46,17 +46,37 @@ def convert_to_datetime(line):
 
        Example dt:  2014-07-03T23:31:22
     """
+    '''
+    Alternative approach:
+    timestamp = line.split()[1]
+    date_str = '%Y-%m-%dT%H:%M:%S'
+    return datetime.strptime(timestamp, date_str)
+    '''
     return datetime.strptime(line.split()[1], '%Y-%m-%dT%H:%M:%S')
 
 
-def time_between_shutdowns(loglines):
+def time_between_shutdowns(loglines: list[str]) -> timedelta:
     """TODO 2:
        Extract shutdown events ("Shutdown initiated") from loglines and
        calculate the timedelta between the first and last one.
        Return this datetime.timedelta object.
     """
-    pass
+    '''
+    Alternative approach:
+    shutdown_entries = [line for line in loglines if SHUTDOWN_EVENT in line]
+    shutdown_times = [convert_to_datetime(event) for event in shutdown_entries]
+    return max(shutdown_times) - min(shutdown_times)  # Nice - independent of temporal ordering!
+    '''
+    events = [logline for logline in loglines if logline.strip().endswith(f'{SHUTDOWN_EVENT}.')]
+    return convert_to_datetime(events[1]) - convert_to_datetime(events[0])
+
+
+# Must be in module scope for tests:
+loglines = get_data()
 
 
 if __name__ == '__main__':
-    loglines = get_data()
+    print(f'The first logline is:\n{loglines[0]}\nThe extracted datetime object is:\n'
+          f'{convert_to_datetime(loglines[0])}')
+    print('\nTime between first and last shutdown initiation:\n'
+          f'{time_between_shutdowns(loglines).seconds:,.2f} seconds.')
