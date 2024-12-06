a12ef2f00a3aaf9775e674ce67da53a6548196c0
James Small <james.r.small@outlook.com>
2024-12-05 21:06:01 -0500

    Update:  Complete generate a table of n sequences code challenge

--- Intermediate/genseqtbl.py
+++ Intermediate/genseqtbl.py
@@ -2,6 +2,7 @@
 
 
 import random
+from typing import Any, Iterator, Sequence
 
 
 names = ['Julian', 'Bob', 'PyBites', 'Dante', 'Martin', 'Rodolfo']
@@ -11,8 +12,14 @@ awake = [True, False] * 3
 SEPARATOR = ' | '
 
 
-def generate_table(*args):
+def generate_table(*args: Sequence[Any]) -> list[str]:
     return [SEPARATOR.join(str(seqelem) for seqelem in zipseq) for zipseq in zip(*args)]
+
+
+def generator_table(*sequences: Sequence[Any]) -> Iterator[str]:
+    for zipseq in zip(*sequences):
+        intseq = [str(val) for val in zipseq]
+        yield SEPARATOR.join(intseq)
 
 
 if __name__ == '__main__':
