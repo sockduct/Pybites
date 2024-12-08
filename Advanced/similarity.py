a92e35a94378660c6b1f999c985c3fdb11f08f81
James Small <james.r.small@outlook.com>
2024-12-07 23:04:06 -0500

    Update:  Complete finding similar words code challenge

--- Advanced/similarity.py
+++ Advanced/similarity.py
@@ -24,23 +24,27 @@ IDENTICAL = 1.0
 SIMILAR = 0.95
 
 
+# Types
+file = Path | str
+
 # Module
-def get_data() -> None:
+def get_data(verbose: bool=False) -> None:
     if not DATADIR.exists():
         DATADIR.mkdir()
 
     if not DATA.exists():
-        print(f'Retrieving data and saving to {DATA}.')
+        if verbose:
+            print(f'Retrieving data and saving to {DATA}.')
         # Retrieve DATA:
         urlretrieve(
             'https://bites-data.s3.us-east-2.amazonaws.com/tags.xml',
             DATA
         )
-    else:
+    elif verbose:
         print(f'{DATA} already present.')
 
 
-def _get_tags(tempfile=TEMPFILE):
+def _get_tags(tempfile: file=TEMPFILE) -> set[str]:
     """Helper to parse all tags from a static copy of PyBites' feed,
        providing this here so you can focus on difflib"""
     with open(tempfile) as f:
@@ -51,17 +55,30 @@ def _get_tags(tempfile=TEMPFILE):
     return set(tags)
 
 
-def get_similarities(tags=None):
+def get_similarities(tags: set[str]|None=None) -> list[tuple[str, str]]:
     """Should return a list of similar tag pairs (tuples)"""
-    tags = tags or _get_tags(DATA)
+    file = DATA if 'DATA' in globals() else TEMPFILE
+    tags = tags or _get_tags(file)
+    '''
+    # Original solution:
+    matches = []
 
     # do your thing ...
     for tag1, tag2 in combinations(tags, 2):
         ratio = SequenceMatcher(None, tag1, tag2).ratio()
         if ratio > SIMILAR:
-            print(f'Hit:  {tag1} is {(100 * ratio):.2f}% simlar to {tag2}')
+            matches.append((tag1, tag2))
+            if verbose:
+                print(f'Hit:  {tag1} is {(100 * ratio):.2f}% simlar to {tag2}')
+    '''
+
+    # Improved solution based on looking at forum:
+    return [
+        pair for pair in combinations(tags, 2)
+        if SequenceMatcher(None, *pair).ratio() > SIMILAR
+    ]
 
 
 if __name__ == '__main__':
-    get_data()
-    get_similarities()
+    get_data(verbose=True)
+    print(get_similarities())
