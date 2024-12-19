c70cfb1dc0e46502d3359e63fb8112a7c1138fe6
James Small <james.r.small@outlook.com>
2024-12-18 21:57:13 -0500

    Update:  Continue building out parse omdb movie... code challenge

--- Intermediate/parsemoviedata.py
+++ Intermediate/parsemoviedata.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3.13
 
 
+from io import StringIO, TextIOWrapper
 import json
 from pathlib import Path
+from typing import Any
 from urllib.request import urlretrieve
 
 
@@ -11,6 +13,10 @@ CWD = Path(__file__).parent
 DATADIR = CWD/'data'
 DATAFILE = 'omdb_data'
 URL = 'https://bites-data.s3.us-east-2.amazonaws.com'
+
+
+# Types
+file = str|Path|TextIOWrapper|StringIO
 
 
 # Module
@@ -31,14 +37,30 @@ def get_data(datafile: str=DATAFILE, datadir: Path=DATADIR, url: str=URL,
         print(f'{data} already present.')
 
 
-def get_movie_data(files: list) -> list:
+def get_movie_data(files: list[file]) -> list[dict[str, Any]]:
     """Parse movie json files into a list of dicts"""
-    pass
+    movie_data = []
+    for json_file in files:
+        # file/path:
+        try:
+            with open(json_file, encoding='utf8') as infile:
+                movie_data.append(json.load(infile))
+        # In-memory buffer:
+        except TypeError:
+            movie_data.append(json.load(json_file))
+
+    return movie_data
 
 
-def get_single_comedy(movies: list) -> str:
-    """return the movie with Comedy in Genres"""
-    pass
+def get_single_comedy(movies: list[dict[str, Any]]) -> str:
+    """
+    return the movie with Comedy in Genres
+    * Title key = movie name
+    * Genre key = list of genres
+    """
+
+    # No error handling!!!
+    return [movie['Title'] for movie in movies if 'Comedy' in movie['Genre']][0]
 
 
 def get_movie_most_nominations(movies: list) -> str:
@@ -53,3 +75,11 @@ def get_movie_longest_runtime(movies: list) -> str:
 
 if __name__ == '__main__':
     get_data(filext='.json')
+
+    files = []
+    with open(f'{DATADIR/DATAFILE}.json', encoding='utf8') as infile:
+        files.extend(StringIO(json_data_line) for json_data_line in infile)
+
+    movie_data = get_movie_data(files)
+    comedy = get_single_comedy(movie_data)
+    ...
