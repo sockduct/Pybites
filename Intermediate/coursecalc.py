ed0a204fc938183dc9deb84a462fec89c197a671
James Small <james.r.small@outlook.com>
2024-12-28 00:59:36 -0500

    Update:  Complete calculate the total duration... code challenge
    * Calculate the total duration of a course

---
+++ Intermediate/coursecalc.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python3.13
+
+
+from datetime import timedelta
+from pathlib import Path
+import re
+from urllib.request import urlretrieve
+
+
+# Global Constants:
+CWD = Path(__file__).parent
+DATADIR = CWD/'data'
+DATAFILE = 'course_timings'
+URL = 'https://bites-data.s3.us-east-2.amazonaws.com'
+
+
+# Module
+### Updates ###
+def get_path(datafile: str=DATAFILE, datadir: Path=DATADIR, filext: str='.txt') -> Path:
+    if not datadir.exists():
+        datadir.mkdir()
+
+    if not (datafilep := Path(datafile)).suffix:
+        datafilep = datafilep.with_suffix(filext)
+
+    return datadir/datafilep
+
+
+def get_data(datafile: str=DATAFILE, url: str=URL, verbose: bool=False) -> None:
+    data_path = get_path()
+    if not data_path.exists():
+        if verbose:
+            print(f'Retrieving data and saving to {data_path}.')
+        urlretrieve(f'{url}/{datafile}', data_path)
+    elif verbose:
+        print(f'{data_path} already present.')
+
+
+def get_all_timestamps(datafile: Path=get_path()) -> list[str]:
+    """Read in the COURSE_TIMES and extract all MM:SS timestamps.
+       Here is a snippet of the input file:
+
+       Start  What is Practical JavaScript? (3:47)
+       Start  The voice in your ear (4:41)
+       Start  Is this course right for you? (1:21)
+       ...
+
+        Return a list of MM:SS timestamps
+    """
+    # course_time_pattern = r'Start\s+[^(]*\((\d{1,2}:\d{2})\)'
+    # Simplify - as simple as possible but no simpler:
+    course_time_pattern = r'\d{1,2}:\d{2}'
+    with open(datafile) as infile:
+        return re.findall(course_time_pattern, infile.read())
+
+
+def parse_mmss(timestamp: str) -> timedelta:
+    minutes, seconds = timestamp.split(':')
+    return timedelta(minutes=int(minutes), seconds=int(seconds))
+
+
+def calc_total_course_duration(timestamps: list[str]) -> str:
+    """Takes timestamps list as returned by get_all_timestamps
+       and calculates the total duration as HH:MM:SS"""
+    # Make sure to review __str__ format before creating your own!
+    return str(
+        sum((parse_mmss(timestamp) for timestamp in timestamps), start=timedelta(seconds=0))
+    )
+
+
+if __name__ == '__main__':
+    get_data(verbose=True)
+    timestamps = get_all_timestamps()
+    print(calc_total_course_duration(timestamps))
---
+++ Intermediate/test/coursecalc_test.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from coursecalc import get_all_timestamps, calc_total_course_duration
+
+
+def test_get_all_timestamps():
+    timestamps = get_all_timestamps()
+    assert len(timestamps) == 99
+    assert '2:29' in timestamps
+    assert '4:19' in timestamps
+    assert '6:06' in timestamps
+    assert '8:39' in timestamps
+
+
+def test_calc_total_course_duration():
+    timestamps = get_all_timestamps()
+    assert '6:50:31' in calc_total_course_duration(timestamps)
