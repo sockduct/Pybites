c0cd4b0ecfb5fafd8c72a19d4721196aad6cbb52
James Small <james.r.small@outlook.com>
2024-12-31 15:36:30 -0500

    Update:  Complete make a bar chart of new safari books code challenge

--- Intermediate/bookbarchart.py
+++ Intermediate/bookbarchart.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3.13
 
 
+from collections import defaultdict
 import os
 from pathlib import Path
-import re
 import sys
 import urllib.request
 
@@ -27,27 +27,38 @@ urllib.request.urlretrieve(
 '''
 
 
-def create_chart():
+def create_chart() -> None:
+    '''
+    Example input:
+    02-13 01:59 root  DEBUG  9781788838542 - WinOps - DevOps on the Microsoft Azure Stack: VSTS and TFS 2018
+    02-13 01:59 root  DEBUG  - sending to slack channel
+    02-13 01:59 root  DEBUG  9781787285217 - Python Web Scraping Cookbook
+    02-13 01:59 root  DEBUG  - cached, skipping
+
+    Goal:
+    * Count the sending to slack channel entries
+      * If previous line was Python book print '🐍' else print '.'
+    '''
     datafile = get_path(datafile=DATA, datadir=DATADIR) if 'DATADIR' in globals() else SAFARI_LOGS
     with open(datafile, encoding='utf8') as infile:
-        books = 0
-        commands = set()
+        output = defaultdict(list)
         for line in infile:
-            # Book starting with ISBN-10/ISBN-13 or 032161INTELLEZY or 200000006A0102
-            if res := re.search(r'\s+DEBUG\s+\d+\w*\s+-\s+', line):
-                books += 1
-            # "Command" line:
-            elif res := re.search(r'\s+DEBUG\s+-\s+([A-Za-z]+)', line, re.IGNORECASE):
-                commands.add(res[1])
-            # Parsing error:
-            else:
-                raise ValueError(f'Unexpected line:  "{line}"')
+            match line.strip().split():
+                case [date, time, 'root', 'DEBUG', idnum, '-', *title]:
+                    book_line = dict(date=date, idnum=idnum, title=title)
+                case [date, time, 'root', 'DEBUG', '-', 'sending', 'to', 'slack', 'channel']:
+                    counter = '🐍' if 'python' in ' '.join(book_line['title']).lower() else '.'
+                    output[book_line['date']].append(counter)
+                case [date, time, 'root', 'DEBUG', '-', 'cached,', 'skipping']:
+                    pass
+                case _:
+                    raise ValueError(f'Unexpected line:  "{line}"')
 
-    print(f'\nBooks:  {books:,}\n'
-          f'Commands:  {", ".join(sorted(commands))}\n')
+        for date, counts in output.items():
+            print(f'{date} {"".join(counts)}')
 
 
 if __name__ == '__main__':
     datapath = get_path(datafile=DATA, datadir=DATADIR)
-    get_data(datafile=DATA, datapath=datapath, verbose=True)
+    get_data(datafile=DATA, datapath=datapath, verbose=False)
     create_chart()
