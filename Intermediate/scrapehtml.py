080bdf8890ddce11e151ae124bcd782265076089
James Small <james.r.small@outlook.com>
2025-01-04 01:07:14 -0500

    Update:  Continue working on scrape packt's html... code challenge

--- Intermediate/scrapehtml.py
+++ Intermediate/scrapehtml.py
@@ -3,6 +3,7 @@
 
 from collections import namedtuple
 from pathlib import Path
+from re import compile, IGNORECASE as ignorecase
 import sys
 
 from bs4 import BeautifulSoup as Soup
@@ -28,10 +29,16 @@ def get_book():
     Goals:
     * Make a Soup object
     * Parse out the relevant fields as defined in Book (from the relevant html sections)
+      * Mastering  Typescript - Second Edition
     * Populate and return a Book namedtuple
     """
-    pass
+    soup = Soup(CONTENT, 'lxml')
+    # title = str(soup.title.string) if soup.title else 'No title found'
+    title = str(soup.find_all(string=compile(r'mastering\s+typescript', ignorecase))[0].strip())
+    description = soup...
 
 if __name__ == '__main__':
     datapath = get_path(datafile=DATAFILE, datadir=DATADIR)
     get_data(datafile=DATAFILE, datapath=datapath, verbose=False)
+    book = get_book()
+    print(book)
