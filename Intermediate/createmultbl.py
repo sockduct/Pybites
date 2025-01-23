7b44d590c69fb727cfa8b2f45f67347b35d995fd
James Small <james.r.small@outlook.com>
2025-01-22 21:06:05 -0500

    Update:  Complete create a multiplication... code challenge

--- Intermediate/createmultbl.py
+++ Intermediate/createmultbl.py
@@ -6,16 +6,15 @@ class MultiplicationTable:
         """Create a 2D self._table of (x, y) coordinates and
            their calculations (form of caching)"""
         self._len = length
-        self._table = [[x * y for x in range(1, length + 1)]
-                       for y in range(1, length + 1)]
+        axis = range(1, length + 1)
+        self._table = [[x * y for x in axis] for y in axis]
 
     def __len__(self) -> int:
         """Returns the area of the table (len x * len y)"""
         return self._len * self._len
 
     def __str__(self) -> str:
-        """Returns a string representation of the table"""
-        w = len(str(self.__len__()))
+        """Returns a basic string representation of the table"""
         str_table = [[str(col) for col in row]
                      for row in self._table]
         return '\n'.join(' | '.join(row) for row in str_table)
@@ -23,14 +22,39 @@ class MultiplicationTable:
     def __repr__(self) -> str:
         return f'MultiplicationTable({self._len})'
 
-    def calc_cell(self, x, y) -> int:
+    def calc_cell(self, x: int, y: int) -> int:
         """Takes x and y coords and returns the re-calculated result"""
-        if 1 > x > self._len or 1 > y > self._len:
-            raise IndexError(f'Expected x and y values between 1 - {self._.len}, got {x}, {y}')
+        if not(1 <= x <= self._len and 1 <= y <= self._len):
+            raise IndexError(f'Expected x and y values between 1 - {self._len}, got {x}, {y}')
 
-        return self._table[y + 1][x + 1]
+        return self._table[y - 1][x - 1]
+
+    def pprint(self) -> str:
+        """Returns prettier string representation of the table"""
+        w = len(str(self.__len__()))
+        output = ''
+        for row in self._table:
+            for pos, col in enumerate(row, 1):
+                output += f'{col:3}'
+                output += ' | ' if pos % self._len != 0 else '\n'
+        return output
 
 
 if __name__ == '__main__':
-    multbl = MultiplicationTable(12)
-    print(multbl)
+    print(f'Basic formatting:\n{MultiplicationTable(4)}')
+    multbl = MultiplicationTable(16)
+    print(f'\nPrettier formatting:\n{multbl.pprint()}')
+    print(multbl.calc_cell(16, 16))
+    try:
+        print(multbl.calc_cell(17, 16))
+    except IndexError as err:
+        print(err)
+    try:
+        print(multbl.calc_cell(0, 5))
+    except IndexError as err:
+        print(err)
+    try:
+        print(multbl.calc_cell(3, -5))
+    except IndexError as err:
+        print(err)
+    print(multbl.calc_cell(11, 8))
