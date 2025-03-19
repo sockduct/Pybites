16dfefecb5c540368ae7c2817f78a51727a276a9
James Small <james.r.small@outlook.com>
2025-03-19 13:21:26 -0400

    Update:  Complete color class...code challenge

--- Intermediate/color.py
+++ Intermediate/color.py
@@ -29,7 +29,7 @@ DATAFILE = 'color_values.py'
 
 sys.path.append(str(Path(DATADIR)))
 try:
-    from color_values import COLOR_NAMES
+    from color_values import COLOR_NAMES  # type: ignore
 except ImportError:
     print(
         'Warning:  Unable to import color_values - try again after running this as the main module.'
@@ -48,8 +48,10 @@ class Color:
         self.rgb = COLOR_NAMES.get(color.upper())
 
     @staticmethod
-    def hex2rgb(hex: str) -> tuple[int, int, int]:
+    def hex2rgb(hex: str) -> tuple[int, ...]:
         """Class method that converts a hex value into an rgb one"""
+        '''
+        # Overkill!!!
         errmsg = 'Expected string of form #[0-9a-f]x6, e.g., "#00ff7e", not "{}".'
         if not isinstance(hex, str) or not re.match(
             r'#[0-9a-fx]{6,12}$', (hex_val := hex.strip().lower())
@@ -62,8 +64,13 @@ class Color:
         while hex_stack:
             current.insert(0, hex_stack.pop())
             match current:
-                case [d1]:
+                case ['#']:
+                    break
+                case [d1] | ['x', d1]:
                     continue
+                case ['0', 'x', d1]:
+                    hex_res.insert(0, f'0{d1}')
+                    current = []
                 case [d1, d2] | ['x', d1, d2]:
                     continue
                 case ['#', d1, d2]:
@@ -80,6 +87,14 @@ class Color:
 
         hex_num = ''.join(hex_res)
         return tuple(int(hex_num[i:i + 2], base=16) for i in (0, 2, 4))
+        '''
+        if not isinstance(hex, str) or not re.match(r'#[0-9a-f]{6}$', (hex_val := hex.strip().lower())):
+            raise ValueError(
+                f'Expected string of form #[0-9a-f]x6, e.g., "#00ff7e", not "{hex}".'
+            )
+
+        return tuple(int(hex_val[i:i + 2], base=16) for i in (1, 3, 5))
+
 
     @staticmethod
     def rgb2hex(rgb: list[int]|tuple[int, int, int]) -> str:
@@ -91,7 +106,7 @@ class Color:
                 f'Expected tuple of 3 integers from 0 - 255, e.g., (0, 127, 255), not "{rgb}".'
             )
 
-        return f"#{''.join(hex(num) for num in rgb)}"
+        return f"#{''.join(f'{num:02x}' for num in rgb)}"
 
     def __repr__(self) -> str:
         """Returns the repl of the object"""
@@ -101,10 +116,13 @@ class Color:
         """Returns the string value of the color object"""
         return f'{self.rgb}' if self.rgb is not None else 'Unknown'
 
-import contextlib
+
 if __name__ == '__main__':
     datapath = get_path(datafile=DATAFILE, datadir=DATADIR)
     get_data(datafile=DATAFILE, datapath=datapath, verbose=True)
+
+    if 'COLOR_NAMES' not in globals():
+        from color_values import COLOR_NAMES
 
     Color('white')
     Color('bad')
@@ -112,16 +130,19 @@ if __name__ == '__main__':
         Color.hex2rgb('bad')
     Color.hex2rgb('#ff00e7')
     Color.hex2rgb('#0xff0x000xe7')
+
+    # Ignore types when intentionally sending invalid values for testing:
     with suppress(ValueError):
-        Color.hex2rgb(1227)
+        Color.hex2rgb(1227)  # type: ignore
     with suppress(ValueError):
-        Color.rgb2hex('abc')
+        Color.rgb2hex('abc')  # type: ignore
     with suppress(ValueError):
-        Color.rgb2hex(122345)
+        Color.rgb2hex(122345)  # type: ignore
     with suppress(ValueError):
-        Color.rgb2hex(['a'])
+        Color.rgb2hex(['a'])  # type: ignore
     with suppress(ValueError):
         Color.rgb2hex([1, 2])
+
     Color.rgb2hex([11, 22, 33])
     print(repr(Color('black')))
     print(Color('white'))
