1eacdf39046629c6f2121b515bf93136b82449ff
James Small <james.r.small@outlook.com>
2025-07-02 23:52:08 -0400

    Update:  Continue working on analyze nba...code challenge

--- Intermediate/nbasql.py
+++ Intermediate/nbasql.py
@@ -7,24 +7,20 @@ from pathlib import Path
 import random
 import sqlite3
 import string
-import sys
+from tempfile import TemporaryDirectory
+from types import ModuleType
 from typing import NamedTuple
 
 import requests
+requests_cache: ModuleType | None
+try:
+    import requests_cache
+except ImportError:
+    requests_cache = None
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from basetmpl import get_data, get_path
-
-
-CWD = Path(__file__).parent
-# Where to store retrieved data:
-DATADIR = CWD/'data'
-# Filename for retrieved data:
-DATAFILE = 'UNDEFINED'
-#
 DATA_URL = 'https://query.data.world/s/ezwk64ej624qyverrw6x7od7co7ftm'
-TMP = Path(os.getenv("TMP", "/tmp"))
+TMP = Path(os.getenv('TMP', '/tmp'))
 
 
 class Player(NamedTuple):
@@ -33,7 +29,7 @@ class Player(NamedTuple):
     first_year: int
     team: str
     college: str
-    active: bool
+    active: int
     games: int
     avg_min: float
     avg_points: float
@@ -42,13 +38,17 @@ class Player(NamedTuple):
 class SQLiteDB:
     _instance = None
     _salt = ''.join(random.choice(string.ascii_lowercase) for i in range(20))
-    _db_path = DATADIR / f'nba_{_salt}.db'
+    _tempdir = TemporaryDirectory()
+    _db_path = Path(_tempdir.name) / f'nba_{_salt}.db'
 
     def __new__(cls, db_path=_db_path):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
             cls._instance._init_connection(db_path)
         return cls._instance
+
+    def __repr__(self):
+        return f'SQLiteDB({self._db_path})'
 
     def _init_connection(self, db_path):
         self._db_path = db_path
@@ -66,44 +66,46 @@ class SQLiteDB:
         self._connection.close()
         SQLiteDB._instance = None  # Allow reinitialization later
 
-'''
-# Usage Example
-db = SQLiteCursorSingleton("my_database.db")
-cursor = db.get_cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)")
-db.commit()
-db.close()
-'''
 
 def import_data():
+    if requests_cache:
+        requests_cache.install_cache('pybites-s3-cache')
     with requests.Session() as session:
         content = session.get(DATA_URL).content.decode('utf-8')
 
     reader = csv.DictReader(content.splitlines(), delimiter=',')
 
     players = []
-    for row in reader:
-        players.append(Player(name=row['Player'],
-                              year=row['Draft_Yr'],
-                              first_year=row['first_year'],
-                              team=row['Team'],
-                              college=row['College'],
-                              active=row['Yrs'],
-                              games=row['Games'],
-                              avg_min=row['Minutes.per.Game'],
-                              avg_points=row['Points.per.Game']))
+    players.extend(
+        Player(
+            name=row['Player'],
+            year=int(row['Draft_Yr']),
+            first_year=int(row['first_year']),
+            team=row['Team'],
+            college=row['College'],
+            active=int(row['Yrs']),
+            games=int(row['Games']),
+            avg_min=float(row['Minutes.per.Game']),
+            avg_points=float(row['Points.per.Game']),
+        )
+        for row in reader
+    )
 
+    db = SQLiteDB()
+    cur = db.get_cursor()
     cur.execute('''CREATE TABLE IF NOT EXISTS players
                   (name, year, first_year, team, college, active,
                   games, avg_min, avg_points)''')
     cur.executemany('INSERT INTO players VALUES (?,?,?,?,?,?,?,?,?)', players)
-    conn.commit()
+    db.commit()
 
 
 def player_with_max_points_per_game():
     """The player with highest average points per game (don't forget to CAST to
        numeric in your SQL query)"""
-    pass
+    cur = SQLiteDB().get_cursor()
+    cur.execute('select name from players order by avg_points desc limit 1')
+    return cur.fetchone()
 
 
 def number_of_players_from_duke():
@@ -124,8 +126,17 @@ def year_with_most_new_players():
     pass
 
 
-if __name__ == '__main__':
-    datapath = get_path(datafile=DATAFILE, datadir=DATADIR)
-    get_data(datafile=DATAFILE, datapath=datapath, verbose=False)
-
+try:
+    # Need to always run the way the tests are designed:
     import_data()
+
+    # Singleton, so OK:
+    db = SQLiteDB()
+    cur = db.get_cursor()
+
+    # Test:
+    print(f'{player_with_max_points_per_game()=}')
+# Ensure orderly close of database and removal of temp directory and files:
+finally:
+    db = SQLiteDB()
+    db.close()
