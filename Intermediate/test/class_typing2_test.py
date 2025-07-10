ffb71e536a723780849948f78aa5f0cfd83ce94b
James Small <james.r.small@outlook.com>
2025-07-09 23:09:43 -0400

    Update:  Complete add type hints to...code challenge

---
+++ Intermediate/test/class_typing2_test.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import sys
+from typing import get_type_hints
+
+import pytest
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from class_typing2 import Employee
+
+
+@pytest.fixture(scope="module")
+def employee():
+    return Employee("Mohhinder", "Suresh", 5, 8, 12.75)
+
+
+def test_employee_type_hints(employee):
+    actual = get_type_hints(employee)
+    expected_type_hints = {
+        "first_name": str,
+        "last_name": str,
+        "days_per_week": int,
+        "hours_per_day": float,
+        "wage": float,
+    }
+    assert actual == expected_type_hints
+
+
+def test_rounder_type_hints(employee):
+    actual = get_type_hints(employee._rounder)
+    expected_type_hints = {
+        "number": float,
+        "places": int,
+        "return": str,
+    }
+    assert actual == expected_type_hints
+
+
+def test_weekly_pay(employee):
+    assert isinstance(employee.weekly_pay, str)
